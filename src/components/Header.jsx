81cd4010d3183bd9d32cf404821659e70d82f8ab
Vova <vivat1996@gmail.com>
2023-03-27 00:12:01 +0200

    React

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTywpc0SQnG6LHbwip9y4y4NpsroxGWFJASLA&usqp=CAU'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://media.cnn.com/api/v1/images/stellar/prod/181010131059-australia-best-beaches-cossies-beach-cocos3.jpg?q=w_1600,h_900,x_0,y_0,c_fill/w_1280'></img>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New posts
+        </div>
+        <div>
+            post 1
+        </div>
+        <div>
+            post 2
+        </div>
+    </div>
+}
+
+export default Profile;
